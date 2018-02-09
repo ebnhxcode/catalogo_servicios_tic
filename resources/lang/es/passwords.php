a7a930698bf17bbf299a776723d92d022346ae80
Esteban Ramos Fernandez <14365041+Estebangramosf@users.noreply.github.com>
2018-02-09 02:51:15 -0300

    update/add - adding spanish lang for alerts, errors, etc.

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'La contraseña debe tener minimo 6 caracteres',
+    'reset' => 'La contraseña ha sido reiniciada!',
+    'sent' => 'Hemos enviado el enlace a tu correo electrónico!',
+    'token' => 'El token de la contraseña es inválido.',
+    'user' => "No hemos podido encontrar tu dirección de correo en nuestros registros.",
+
+];
