8cc5a47878371733713f7dcfedf4470c3e2d9b60
Esteban Ramos Fernandez <14365041+Estebangramosf@users.noreply.github.com>
2018-02-17 23:12:45 -0300

    update/add - adding details for table aplicaciones.

--- app/Aplicacion.php
+++ app/Aplicacion.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Aplicacion extends Model
 {
-    //
+   protected $table = "aplicaciones";
+   protected $primaryKey = "id";
+   protected $fillable = [
+      #columns
+      'nom_aplicacion',
+      'det_aplicacion',
+      'alias',
+      'url_web',
+      'ip',
+      'subdominio',
+      'ssl_tls',
+      '',
+      '',
+
+      #relaciones -> pks
+      'id_dominio',
+      'id_servicio',
+      'id_tipo_actividad',
+      '',
+   ];
+
 }
