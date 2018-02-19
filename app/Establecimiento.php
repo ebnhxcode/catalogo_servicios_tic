d6b34e8e07c7292c67e14162d5de9a8e5cc88f79
Esteban Ramos Fernandez <14365041+Estebangramosf@users.noreply.github.com>
2018-02-18 23:53:23 -0300

    update/add - fix and repair and completing establecimiento model.

--- app/Establecimiento.php
+++ app/Establecimiento.php
@@ -11,7 +11,6 @@ class Establecimiento extends Model
    protected $fillable = [
       # columns
       'nombre_establecimiento',
-      'nombre_establecimiento',
       'tipo_establecimiento',
       'vigencia_desde',
       'fecha_cierre',
@@ -24,9 +23,20 @@ class Establecimiento extends Model
       'id_comuna',
    ];
 
+   public function establecimiento_antiguo() {
+      return $this->belongsTo('App\Establecimiento', 'id_establecimiento_antiguo');
+   }
 
+   public function servicio_salud() {
+      return $this->belongsTo('App\ServicioSalud', 'id_servicio_salud');
+   }
 
+   public function region() {
+      return $this->belongsTo('App\Region', 'id_region');
+   }
 
-
+   public function comuna() {
+      return $this->belongsTo('App\Comuna', 'id_comuna');
+   }
 
 }
