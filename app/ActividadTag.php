a9bb19760b390051eea450e4257a68790a1e9c01
Esteban Ramos Fernandez <14365041+Estebangramosf@users.noreply.github.com>
2018-02-19 10:19:47 -0300

    update/add - adding and completing actividad tag.

--- app/ActividadTag.php
+++ app/ActividadTag.php
@@ -4,20 +4,28 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Actividad extends Model
+class ActividadTag extends Model
 {
-   protected $table = "actividades";
-   protected $primaryKey = "id_actividad";
+   protected $table = "actividades_tags";
+   protected $primaryKey = "id_actividad_tag";
    protected $fillable = [
       #columns
-      'nom_actividad',
-      'det_actividad',
 
       #relaciones -> pks
+      'id_actividad',
+      'id_tag',
 
       'id_usuario_registra',
       'id_usuario_modifica',
    ];
+
+   public function actividad () {
+      return $this->belongsTo('App\Actividad', 'id_actividad');
+   }
+
+   public function tag () {
+      return $this->belongsTo('App\Tag', 'id_tag');
+   }
 
    public function usuario_registra() {
       return $this->belongsTo('App\User', 'id_usuario_registra');
