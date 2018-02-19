dc5fdac930546bdd49722b3f949bfd4aaeb23bb0
Esteban Ramos Fernandez <14365041+Estebangramosf@users.noreply.github.com>
2018-02-18 22:48:19 -0300

    update/add - complete model catalogo servicio

--- app/CatalogoServicio.php
+++ app/CatalogoServicio.php
@@ -6,5 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class CatalogoServicio extends Model
 {
-    //
+   protected $table = "catalogos_servicios";
+   protected $primaryKey = "id_catalogo_servicio";
+   protected $fillable = [
+      #columns
+      'asunto',
+      'det_catalogo',
+      'avatar',
+
+      #relaciones -> pks
+      'id_servicio',
+
+      'id_usuario_registra',
+      'id_usuario_modifica',
+   ];
+
+   public function servicio() {
+      return $this->belongsTo('App\Servicio', 'id_servicio');
+   }
+
+   public function usuario_registra() {
+      return $this->belongsTo('App\User', 'id_usuario_registra');
+   }
+
+   public function usuario_modifica() {
+      return $this->belongsTo('App\User', 'id_usuario_modifica');
+   }
 }
