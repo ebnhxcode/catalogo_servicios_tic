8f2f5e208094011b9758001b4259aa8e3789fb27
Esteban Ramos Fernandez <14365041+Estebangramosf@users.noreply.github.com>
2018-02-19 10:14:38 -0300

    update/add - hidrating models usuario role and usuario servicio.

--- app/UsuarioRole.php
+++ app/UsuarioRole.php
@@ -6,5 +6,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class UsuarioRole extends Model
 {
-    //
+   protected $table = "usuario_role";
+   protected $primaryKey = "id_usuario_role";
+   protected $fillable = [
+      #columns
+
+      #relaciones -> pks
+      'id_role',
+      'id_usuario',
+
+      'id_usuario_registra',
+      'id_usuario_modifica',
+   ];
+
+   public function role () {
+      return $this->belongsTo('App\Role', 'id_role');
+   }
+
+   public function usuario () {
+      return $this->belongsTo('App\User', 'id_usuario');
+   }
+
+   public function usuario_registra() {
+      return $this->belongsTo('App\User', 'id_usuario_registra');
+   }
+
+   public function usuario_modifica() {
+      return $this->belongsTo('App\User', 'id_usuario_modifica');
+   }
 }
--- app/UsuarioServicio.php
+++ app/UsuarioServicio.php
@@ -6,5 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class UsuarioServicio extends Model
 {
-    //
+   protected $table = "usuario_servicio";
+   protected $primaryKey = "id_usuario_servicio";
+   protected $fillable = [
+      #columns
+      #relaciones -> pks
+      'id_usuario',
+      'id_servicio',
+
+      'id_usuario_registra',
+      'id_usuario_modifica',
+   ];
+
+   public function usuario () {
+      return $this->belongsTo('App\User', 'id_usuario');
+   }
+
+   public function servicio () {
+      return $this->belongsTo('App\Servicio', 'id_servicio');
+   }
+
+   public function usuario_registra() {
+      return $this->belongsTo('App\User', 'id_usuario_registra');
+   }
+
+   public function usuario_modifica() {
+      return $this->belongsTo('App\User', 'id_usuario_modifica');
+   }
 }
