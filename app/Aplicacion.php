30928ee75dc81aa77e7b27ed8eef47e8faf2f51e
Esteban Ramos Fernandez <14365041+Estebangramosf@users.noreply.github.com>
2018-02-18 22:28:30 -0300

    update/add - complete model aplicacion.

--- app/Aplicacion.php
+++ app/Aplicacion.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Aplicacion extends Model
 {
    protected $table = "aplicaciones";
-   protected $primaryKey = "id";
+   protected $primaryKey = "id_aplicacion";
    protected $fillable = [
       #columns
       'nom_aplicacion',
@@ -17,14 +17,35 @@ class Aplicacion extends Model
       'ip',
       'subdominio',
       'ssl_tls',
-      '',
-      '',
 
       #relaciones -> pks
       'id_dominio',
       'id_servicio',
-      'id_tipo_actividad',
-      '',
+      'id_tipo_aplicacion',
+      'id_usuario_registra',
+      'id_usuario_modifica',
    ];
+
+   public function dominio() {
+      return $this->belongsTo('App\Dominio', 'id_dominio');
+   }
+
+   public function servicio() {
+      return $this->belongsTo('App\Servicio', 'id_servicio');
+   }
+
+   public function tipo_aplicacion() {
+      return $this->belongsTo('App\Servicio', 'id_tipo_aplicacion');
+   }
+
+   public function usuario_registra() {
+      return $this->belongsTo('App\User', 'id_usuario_registra');
+   }
+
+   public function usuario_modifica() {
+      return $this->belongsTo('App\User', 'id_usuario_modifica');
+   }
+
+
 
 }
