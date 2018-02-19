5f3f009b7e9094bf32eedf1e76712079431105ef
Esteban Ramos Fernandez <14365041+Estebangramosf@users.noreply.github.com>
2018-02-19 12:28:59 -0300

    update/add - adding moddleware jefe area, app manager, jefe proyecto, desarrollador proyecto, visitante.

---
+++ app/Http/Middleware/Role/JefeProyecto.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware\Role;
+
+use Closure;
+
+class JefeProyecto
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        return $next($request);
+    }
+}
