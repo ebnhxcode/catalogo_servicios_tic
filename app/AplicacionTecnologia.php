eb24db21e966a543cb0f08ce0a0409473e1b6de8
Esteban Ramos Fernandez <14365041+Estebangramosf@users.noreply.github.com>
2018-02-18 22:46:28 -0300

    update/add - complete model aplicacion tecnologia

--- app/AplicacionTecnologia.php
+++ app/AplicacionTecnologia.php
@@ -6,5 +6,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class AplicacionTecnologia extends Model
 {
-    //
+   protected $table = "aplicaciones_tecnologia";
+   protected $primaryKey = "id_aplicacion_tecnologia";
+   protected $fillable = [
+      #columns
+
+      #relaciones -> pks
+      'id_aplicacion',
+      'id_tecnologia',
+
+      'id_usuario_registra',
+      'id_usuario_modifica',
+   ];
+
+   public function aplicacion() {
+      return $this->belongsTo('App\Aplicacion', 'id_aplicacion');
+   }
+
+   public function tecnologia() {
+      return $this->belongsTo('App\TecnologiaDesarrollo', 'id_tecnologia');
+   }
+
+   public function usuario_registra() {
+      return $this->belongsTo('App\User', 'id_usuario_registra');
+   }
+
+   public function usuario_modifica() {
+      return $this->belongsTo('App\User', 'id_usuario_modifica');
+   }
 }
