a7f085e1b0c6068a0741b1151b6dfd823fb0c6c0
Esteban Ramos Fernandez <14365041+Estebangramosf@users.noreply.github.com>
2018-02-26 13:28:27 -0300

    update/add - templating some parts of code, searching reutilize in max. the code.

--- resources/views/roles/main.blade.php
+++ resources/views/roles/main.blade.php
@@ -30,7 +30,11 @@
 
                   <div class="dropdown-menu">
 
-                     <!-- Acá : modificaciones únicas de negocio -->
+                     <!-- ############################################# -->
+                     <!-- Acá : Inicio modificaciones únicas de negocio -->
+                     <!-- ############################################# -->
+
+                     <!-- Esta seccion ya es un componente, se podria estandarizar solo el nombre de los obj para excel -->
                      <download-excel
                         v-if="(excel_data_contador = filterBy(roles, filtro_head).length) > 0"
                         :data="filterBy(roles, filtro_head)"
@@ -39,6 +43,10 @@
                         class="dropdown-item">
                         Descargar Excel
                      </download-excel>
+
+                     <!-- ############################################ -->
+                     <!-- Acá : Final modificaciones únicas de negocio -->
+                     <!-- ############################################ -->
 
                      <a class="dropdown-item" href="#!" @click.prevent="inicializar">
                         Actualizar tabla
@@ -62,21 +70,38 @@
       {{--<canvas class="my-4" id="myChart" width="900" height="380"></canvas>--}}
 
 
-         <!-- Acá : modificaciones únicas de negocio -->
 
-      <h5 style="position: relative;">Mostrar\Ocultar opciones de la tabla
-         (<small>Clic en un botón para mostrar en grilla</small>)</h5>
-      {{--<mini-spinner v-if="mini_spinner_table_inputs == true"></mini-spinner> v-else --}}
-      <div class="btn-group btn-group-sm btn-group-toggle" v-for="v,c,i in tabla_roles_campos">
-         <label :class="v==true?'btn btn-primary active':'btn btn-secondary'" @click.prevent="cambiar_visibilidad(c)"
-                {{--data-placement="top" data-toggle="tooltip" :title="`Clic para ${(v==true)?'ocultar':'mostrar'}`">--}}
-                data-placement="top" data-toggle="tooltip" title="Clic para mostrar u ocultar">
-            <input type="checkbox" autocomplete="off"> <span style="font-size: 85%;">@{{ tabla_roles_labels[c] }}</span>
-         </label>
-      </div>
+
+
+      <!-- ############################################# -->
+      <!-- Acá : Inicio modificaciones únicas de negocio -->
+      <!-- ############################################# -->
+
+      <!-- Esto puede ser pasado como componente, se le pasa el objeto en json con los campos de la tabla -->
+      <template>
+         <h5 style="position: relative;">Mostrar\Ocultar opciones de la tabla
+            (<small>Clic en un botón para mostrar en grilla</small>)</h5>
+         {{--<mini-spinner v-if="mini_spinner_table_inputs == true"></mini-spinner> v-else --}}
+         <div class="btn-group btn-group-sm btn-group-toggle" v-for="v,c,i in tabla_roles_campos">
+            <label :class="v==true?'btn btn-primary active':'btn btn-secondary'" @click.prevent="cambiar_visibilidad(c)"
+                   {{--data-placement="top" data-toggle="tooltip" :title="`Clic para ${(v==true)?'ocultar':'mostrar'}`">--}}
+                   data-placement="top" data-toggle="tooltip" title="Clic para mostrar u ocultar">
+               <input type="checkbox" autocomplete="off"> <span style="font-size: 85%;">@{{ tabla_roles_labels[c] }}</span>
+            </label>
+         </div>
+      </template>
       <br />
       <br />
 
+      <!-- ############################################ -->
+      <!-- Acá : Final modificaciones únicas de negocio -->
+      <!-- ############################################ -->
+
+
+
+      <!-- %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% -->
+      <!-- Acá : Esta seccion puede ser completamente heredable como body del main -->
+      <!-- %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% -->
 
       <h4 class="h4">Lista de {{$nombre_detalle}}</h4>
       <!-- Sección de la tabla que lista los elementos del módulo -->
@@ -95,6 +120,11 @@
       {{--<notifications group="top_center" position="top center" />--}}
       {{--<v-dialog/>--}}
       <notifications group="global" position="bottom right" />
+
+      <!-- Para cada modulo establecer estas funcionalidades parametricas como opciones configurables -->
+      <!-- Por lo tanto podemos dejar estas opciones como datos en una bd segun modulo:usuario -->
+      <!-- Opciones como colores, templates especiales, etc -->
+
 
 
    </main>
