4df9443e0895cea3106283a475767acd34e5b3f1
Esteban Ramos Fernandez <14365041+Estebangramosf@users.noreply.github.com>
2018-02-27 10:44:17 -0300

    update/add - fix controllers for users, pw validation.

--- resources/assets/js/controllers/SideMenuController.js
+++ resources/assets/js/controllers/SideMenuController.js
@@ -49,7 +49,7 @@ Validator.extend('verify_password', {
    y que sea mínimo de 8 caracteres
    `,
    validate: value => {
-      var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+      var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,20})");
       return strongRegex.test(value);
    }
 });
