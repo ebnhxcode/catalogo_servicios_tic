92c425783c6e9184bc95f86a7e4b744b39f4e7ca
Esteban Ramos Fernandez <14365041+Estebangramosf@users.noreply.github.com>
2018-03-09 01:30:57 -0300

    update/add - giving format for tags.

--- resources/views/tags/partials/formulario_campos.blade.php
+++ resources/views/tags/partials/formulario_campos.blade.php
@@ -1,5 +1,6 @@
+<h5>Datos a completar para administrar tags</h5>
 <div class="row">
-   <div class="col-sm-6 col-md-6">
+   <div class="col-sm-4 col-md-4">
 
       <dt>Nombre tag</dt>
       <dd>
@@ -21,7 +22,29 @@
       </dd>
 
    </div><!-- .col -->
-   <div class="col-sm-6 col-md-6">
+   <div class="col-sm-4 col-md-4">
+
+      <dt>Meta tag (opcional como metadato)</dt>
+      <dd>
+         <p class="control has-icon has-icon-right">
+            <input type="text" v-model="tag.meta_tag" name="meta_tag"
+                   v-validate="{regex:/^[a-zA-Z0-9_ ]+$/i}" data-vv-delay="500"
+                   class="form-control" />
+
+            <transition name="bounce">
+               <i v-show="errors.has('meta_tag')" class="fa fa-exclamation-circle"></i>
+            </transition>
+
+            <transition name="bounce">
+               <span v-show="errors.has('meta_tag')" class="text-danger small">
+                  @{{ errors.first('meta_tag') }}
+               </span>
+            </transition>
+         </p>
+      </dd>
+
+   </div><!-- .col -->
+   <div class="col-sm-4 col-md-4">
 
       <dt>Detalle tag</dt>
       <dd>
@@ -43,26 +66,5 @@
       </dd>
 
    </div><!-- .col -->
-   <div class="col-sm-6 col-md-6">
 
-      <dt>Meta tag</dt>
-      <dd>
-         <p class="control has-icon has-icon-right">
-            <input type="text" v-model="tag.meta_tag" name="meta_tag"
-                   v-validate="{regex:/^[a-zA-Z0-9_ ]+$/i}" data-vv-delay="500"
-                   class="form-control" />
-
-            <transition name="bounce">
-               <i v-show="errors.has('meta_tag')" class="fa fa-exclamation-circle"></i>
-            </transition>
-
-            <transition name="bounce">
-               <span v-show="errors.has('meta_tag')" class="text-danger small">
-                  @{{ errors.first('meta_tag') }}
-               </span>
-            </transition>
-         </p>
-      </dd>
-
-   </div><!-- .col -->
 </div><!-- .row -->
