e48734226f4e922f5281895566a0ef43af739ade
Esteban Ramos Fernandez <14365041+Estebangramosf@users.noreply.github.com>
2018-03-09 01:46:32 -0300

    update/add - giving format for estados.

--- resources/views/estados/partials/formulario_campos.blade.php
+++ resources/views/estados/partials/formulario_campos.blade.php
@@ -1,6 +1,6 @@
-
+<h5>Datos básicos para administrar permisos</h5>
 <div class="row">
-   <div class="col-sm-6 col-md-6">
+   <div class="col-sm-4 col-md-4">
 
       <dt>Nombre estado</dt>
       <dd>
@@ -23,30 +23,8 @@
 
 
    </div><!-- .col -->
-   <div class="col-sm-6 col-md-6">
 
-      <dt>Detalle estado</dt>
-      <dd>
-
-         <p class="control has-icon has-icon-right">
-            <textarea cols="15" rows="1" v-model="estado.det_estado" name="det_estado"
-                      v-validate="{required:true,regex:/^[a-zA-Z0-9_ ,.!@#$%*&]+$/i}" data-vv-delay="500"
-                      class="form-control"></textarea>
-
-            <transition name="bounce">
-               <i v-show="errors.has('det_estado')" class="fa fa-exclamation-circle"></i>
-            </transition>
-
-            <transition name="bounce">
-               <span v-show="errors.has('det_estado')" class="text-danger small">
-                  @{{ errors.first('det_estado') }}
-               </span>
-            </transition>
-         </p>
-      </dd>
-
-   </div><!-- .col -->
-   <div class="col-sm-6 col-md-6">
+   <div class="col-sm-4 col-md-4">
 
       <dt>Codigo estado</dt>
       <dd>
@@ -69,5 +47,30 @@
       </dd>
 
    </div><!-- .col -->
+
+   <div class="col-sm-4 col-md-4">
+
+      <dt>Detalle estado</dt>
+      <dd>
+
+         <p class="control has-icon has-icon-right">
+            <textarea cols="15" rows="1" v-model="estado.det_estado" name="det_estado"
+                      v-validate="{required:true,regex:/^[a-zA-Z0-9_ ,.!@#$%*&]+$/i}" data-vv-delay="500"
+                      class="form-control"></textarea>
+
+            <transition name="bounce">
+               <i v-show="errors.has('det_estado')" class="fa fa-exclamation-circle"></i>
+            </transition>
+
+            <transition name="bounce">
+               <span v-show="errors.has('det_estado')" class="text-danger small">
+                  @{{ errors.first('det_estado') }}
+               </span>
+            </transition>
+         </p>
+      </dd>
+
+   </div><!-- .col -->
+
 
 </div><!-- .row -->
