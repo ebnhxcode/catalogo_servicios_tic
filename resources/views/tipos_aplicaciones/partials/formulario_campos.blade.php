576326a40c7ae286f0ab76dfa0f85961492dc9d6
Esteban Ramos Fernandez <14365041+Estebangramosf@users.noreply.github.com>
2018-03-09 01:27:54 -0300

    update/add - giving format form tipos aplicaciones.

--- resources/views/tipos_aplicaciones/partials/formulario_campos.blade.php
+++ resources/views/tipos_aplicaciones/partials/formulario_campos.blade.php
@@ -1,6 +1,7 @@
 
+<h5>Datos a completar sobre tipo de aplicación</h5>
 <div class="row">
-   <div class="col-sm-6 col-md-6">
+   <div class="col-sm-4 col-md-4">
 
       <dt>Nombre tipo aplicacion</dt>
       <dd>
@@ -23,7 +24,32 @@
 
 
    </div><!-- .col -->
-   <div class="col-sm-6 col-md-6">
+
+   <div class="col-sm-4 col-md-4">
+
+      <dt>Codigo tipo aplicacion (opcional)</dt>
+      <dd>
+
+         <p class="control has-icon has-icon-right">
+            <input type="text" v-model="tipo_aplicacion.cod_tipo_aplicacion" name="cod_tipo_aplicacion"
+                   v-validate="{regex:/^[a-zA-Z0-9_ ,.!@#$%*&]+$/i}" data-vv-delay="500"
+                   class="form-control" />
+
+            <transition name="bounce">
+               <i v-show="errors.has('cod_tipo_aplicacion')" class="fa fa-exclamation-circle"></i>
+            </transition>
+
+            <transition name="bounce">
+               <span v-show="errors.has('cod_tipo_aplicacion')" class="text-danger small">
+                  @{{ errors.first('cod_tipo_aplicacion') }}
+               </span>
+            </transition>
+         </p>
+      </dd>
+
+   </div><!-- .col -->
+
+   <div class="col-sm-4 col-md-4">
 
       <dt>Detalle tipo aplicacion</dt>
       <dd>
@@ -40,29 +66,6 @@
             <transition name="bounce">
                <span v-show="errors.has('det_tipo_aplicacion')" class="text-danger small">
                   @{{ errors.first('det_tipo_aplicacion') }}
-               </span>
-            </transition>
-         </p>
-      </dd>
-
-   </div><!-- .col -->
-   <div class="col-sm-6 col-md-6">
-
-      <dt>Codigo tipo aplicacion</dt>
-      <dd>
-
-         <p class="control has-icon has-icon-right">
-            <input type="text" v-model="tipo_aplicacion.cod_tipo_aplicacion" name="cod_tipo_aplicacion"
-                   v-validate="{regex:/^[a-zA-Z0-9_ ,.!@#$%*&]+$/i}" data-vv-delay="500"
-                   class="form-control" />
-
-            <transition name="bounce">
-               <i v-show="errors.has('cod_tipo_aplicacion')" class="fa fa-exclamation-circle"></i>
-            </transition>
-
-            <transition name="bounce">
-               <span v-show="errors.has('cod_tipo_aplicacion')" class="text-danger small">
-                  @{{ errors.first('cod_tipo_aplicacion') }}
                </span>
             </transition>
          </p>
