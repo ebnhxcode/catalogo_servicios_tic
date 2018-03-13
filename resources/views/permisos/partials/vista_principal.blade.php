d0cf921062cd92ee56ad7cf37a760bc02a85d0a4
Esteban Ramos Fernandez <14365041+Estebangramosf@users.noreply.github.com>
2018-03-13 10:37:52 -0300

    update/add - adding vista principal to permiso

--- resources/views/permisos/partials/vista_principal.blade.php
+++ resources/views/permisos/partials/vista_principal.blade.php
@@ -4,12 +4,37 @@
 
    <div class="row">
       <div class="col-sm-4 col-md-4">
-         <h1>Vista principal</h1>
+
+         <!-- este bloque será reemplazado dinamicamente -->
+         <div class="card" style="{{--width: 18rem;--}}">
+            <img class="card-img-top" src="{{ url('/img/permiso.jpg') }}" alt="Card image cap">
+            <div class="card-body">
+               <h5 class="card-title">
+                  @{{ permiso.nom_permiso || '' }}
+               </h5>
+               <p class="card-text">
+
+               <dl class="row" v-if="permiso">
+                  <dd class="col-md-12">@{{ permiso.det_permiso || '' }}</dd>
+
+                  <dt class="col-md-6">Código permiso</dt>
+                  <dd class="col-md-6">@{{ permiso.cod_permiso || '' }}</dd>
+               </dl>
+
+               <dl v-else>
+                  No hay información del permiso.
+               </dl>
+
+               </p>
+               {{--<a href="#" class="btn btn-primary">Go somewhere</a>--}}
+            </div><!-- .card-body -->
+         </div><!-- .card -->
+
+         <br>
 
       </div><!-- .col -->
+
+
    </div><!-- .row -->
-
-
-
 
 </div><!-- .tab-pane .active #vista_principal_tab -->
