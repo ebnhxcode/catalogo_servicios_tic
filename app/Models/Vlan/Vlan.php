c166eab517c3eedaa5c89d7f63b5472a2149f363
Esteban Ramos Fernandez <14365041+Estebangramosf@users.noreply.github.com>
2018-04-07 10:39:55 -0300

    update/add - finish model to vlans.

--- app/Models/Vlan/Vlan.php
+++ app/Models/Vlan/Vlan.php
@@ -3,7 +3,32 @@
 namespace App\Models\Vlan;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Vlan extends Model {
+
+   use SoftDeletes;
+   protected $dates = ['deleted_at'];
+
+   protected $table = "vlans";
+   protected $primaryKey = "id_vlan";
+   protected $fillable = [
+      #columns
+      'nom_vlan',
+      'det_vlan',
+      'cod_vlan',
+
+      #relaciones -> pks
+      'id_usuario_registra',
+      'id_usuario_modifica',
+   ];
+
+   public function usuario_registra() {
+      return $this->belongsTo(User::class, 'id_usuario_registra');
+   }
+
+   public function usuario_modifica() {
+      return $this->belongsTo(User::class, 'id_usuario_modifica');
+   }
 
 }
