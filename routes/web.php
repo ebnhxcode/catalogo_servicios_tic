106cf8c12228fb7bdc91526c3486d6195f7da885
Esteban Ramos Fernandez <esteban.ramos.f@gmail.com>
2018-05-04 15:46:53 -0300

    update/add - adding documentation in routes.

--- routes/web.php
+++ routes/web.php
@@ -39,17 +39,26 @@ Route::get('/', function () {
 #Route::get('/embed/tipos_servidores', 'TipoServidorController@index_componente');
 
 #Rutas para los ajax de cada index.
-
 Route::get('/ajax/servicios', 'ServicioController@index_ajax');
 Route::get('/ajax/servidores', 'ServidorController@index_ajax');
 Route::get('/ajax/aplicaciones', 'AplicacionController@index_ajax');
 
 
-#Esto aplica solo para las rutas que eran resource , es una forma de mapear las url/rutas
-$groups = ['r','c','u','d'];
 
+/*
+ * Modulos :
+ * Se define arreglo (de módulos) con las rutas de recursos para crud básico (resources)
+ * Y el controller asociado a esta ruta
+ *
+ *    En este caso :
+ *
+ *
+ * */
 $modules = [
-    ['route'=>'roles','controller'=>'RoleController'],
+    [
+       'route'=>'roles',
+       'controller'=>'RoleController'
+    ],
     ['route'=>'permisos','controller'=>'PermisoController'],
     ['route'=>'usuarios','controller'=>'UsuarioController'],
     ['route'=>'servicios','controller'=>'ServicioController'],
@@ -75,11 +84,99 @@ $modules = [
     ['route'=>'vlans','controller'=>'VlanController'],
 ];
 
+
+/*
+ * Grupos :
+ * Se define arreglo (de grupos) con los middlewares que serán distribuídos los recursos
+ * Esto aplica solo para las rutas que eran resource , es una forma de mapear las url/rutas
+ *
+ *    En este caso :
+ *
+ *    r = read -> nivel 1
+ *       :-> por defecto: nada ;
+ *       :-> adicional: puede leer -> recursos del sistema
+ *
+ *    c = create -> nivel 2
+ *       :-> por defecto: puede leer ;
+ *       :-> adicional: puede crear -> recursos del sistema
+ *
+ *    u = update -> nivel 3
+ *       :-> por defecto: puede leer y crear ;
+ *       :-> adicional: puede modificar -> recursos del sistema
+ *
+ *    d = delete -> nivel 4
+ *       :-> por defecto: puede leer, crear y modificar ;
+ *       :-> adicional: puede eliminar -> recursos del sistema
+ *
+ * */
+$groups = ['r','c','u','d'];
+
+#Docs
+/*
+ * Peticiones Http
+ * Se define arreglo (de métodos y tipo de peticiones) asociados a cada grupo de middlewares
+ *
+ * Path de middlewares definidos para este aplicativo -> en Http/Middleware/Role/*
+ *
+ * ***
+ *
+ *    En caso 'r':
+ *
+ *    El middleware RMiddleware (Registrado en el Kernel como => 'r')
+ *    Tiene asociado el método 'index' y 'show', ya que 'r' representa a 'read',
+ *    y read es una peticion http de tipo get|head que es para solo lectura.
+ *
+ *    Finalmente, con esto se logra que para cada controlador, se autodefina una ruta get para los metodos index|show.
+ *
+ * ***
+ *
+ *    En caso 'c':
+ *
+ *    El middleware CMiddleware (Registrado en el Kernel como => 'c')
+ *    Tiene asociado el método 'store', ya que 'c' representa a 'create',
+ *    y store es una peticion http de tipo post que es para escribir recursos en el servidor.
+ *
+ *    Finalmente, con esto se logra que para cada controlador, se autodefina una ruta post para el metodo store.
+ *
+ * ***
+ *
+ *    En caso 'u':
+ *
+ *    El middleware UMiddleware (Registrado en el Kernel como => 'u')
+ *    Tiene asociado el método 'update', ya que 'u' representa a 'update',
+ *    y store es una peticion http de tipo put|patch que es para sobreescribir recursos en el servidor.
+ *
+ *    Finalmente, con esto se logra que para cada controlador, se autodefina una ruta put|patch para el metodo update.
+ *
+ * ***
+ *
+ *    En caso 'd':
+ *
+ *    El middleware DMiddleware (Registrado en el Kernel como => 'd')
+ *    Tiene asociado el método 'destroy', ya que 'd' representa a 'delete',
+ *    y delete es una peticion http de tipo delete que es para eliminar recursos en el servidor.
+ *
+ *    Finalmente, con esto se logra que para cada controlador, se autodefina una ruta delete para el metodo destroy.
+ *
+ *
+ *
+ * Se pueden incluir todos los métodos que son compartidos entre controladores y rutas (preferentemente rutas de recursos)
+ * */
 $http_requests = [
-   'r' => ['index' => 'get','show' => 'get'],
-   'c' => ['store' => 'post',],
-   'u' => ['update' => 'put',],
-   'd' => ['destroy' => 'delete',],
+   'r' => [
+      'index' => 'get',
+      'index_ajax' => 'get',
+      'show' => 'get'
+   ],
+   'c' => [
+      'store' => 'post'
+   ],
+   'u' => [
+      'update' => 'put'
+   ],
+   'd' => [
+      'destroy' => 'delete'
+   ]
 ];
 
 foreach ($groups as $group) {
@@ -89,125 +186,3 @@ foreach ($groups as $group) {
                eval("Route::${http}('/${module['route']}/".(in_array($method,["show","update","destroy"])?"{id}":"")."', '${module['controller']}@${method}');");
     });
 }
-
-#$route_arg = in_array($method,["show","update","destroy"])?"{id}":"";
-#$route = "Route::${http}('/${module['route']}/".$route_arg."', '${module['controller']}@${method}');";
-#eval($route);
-
-
-
-/*
-
-Route::group(['middleware' => 'r'], function(){
-
-    //Route::resource('/catalogo_servicios', 'CatalogoServicioController');
-    Route::get('/roles', 'RoleController@index');
-    Route::get('/permisos', 'PermisoController@index');
-    Route::get('/usuarios', 'UsuarioController@index');
-    Route::get('/servicios', 'ServicioController@index');
-    Route::get('/actividades', 'ActividadController@index');
-    Route::get('/cargos', 'CargoController@index');
-    Route::get('/estados', 'EstadoController@index');
-    Route::get('/aplicaciones', 'AplicacionController@index');
-    Route::get('/tipos_aplicaciones', 'TipoAplicacionController@index');
-    Route::get('/aplicaciones_accesos', 'AplicacionAccesoController@index');
-    Route::get('/dominios', 'DominioController@index');
-    Route::get('/servidores', 'ServidorController@index');
-    Route::get('/servidores_accesos', 'ServidorAccesoController@index');
-    Route::get('/datacentros', 'DatacentroController@index');
-    Route::get('/sistemas_operativos', 'SistemaOperativoController@index');
-    Route::get('/tags', 'TagController@index');
-    Route::get('/usuarios_bitacora_servicios', 'UsuarioBitacoraServicioController@index');
-    Route::get('/clusters', 'ClusterController@index');
-
-    Route::get('/roles/{id}', 'RoleController@show');
-    Route::get('/permisos/{id}', 'PermisoController@show');
-    Route::get('/usuarios/{id}', 'UsuarioController@show');
-    Route::get('/servicios/{id}', 'ServicioController@show');
-    Route::get('/actividades/{id}', 'ActividadController@show');
-    Route::get('/cargos/{id}', 'CargoController@show');
-    Route::get('/estados/{id}', 'EstadoController@show');
-    Route::get('/aplicaciones/{id}', 'AplicacionController@show');
-    Route::get('/tipos_aplicaciones/{id}', 'TipoAplicacionController@show');
-    Route::get('/aplicaciones_accesos/{id}', 'AplicacionAccesoController@show');
-    Route::get('/dominios/{id}', 'DominioController@show');
-    Route::get('/servidores/{id}', 'ServidorController@show');
-    Route::get('/servidores_accesos/{id}', 'ServidorAccesoController@show');
-    Route::get('/datacentros/{id}', 'DatacentroController@show');
-    Route::get('/sistemas_operativos/{id}', 'SistemaOperativoController@show');
-    Route::get('/tags/{id}', 'TagController@show');
-    Route::get('/usuarios_bitacora_servicios/{id}', 'UsuarioBitacoraServicioController@show');
-    Route::get('/clusters/{id}', 'ClusterController@show');
-
-});
-
-Route::group(['middleware' => ['c']], function(){
-    //Route::resource('/catalogo_servicios', 'CatalogoServicioController');
-    Route::post('/roles', 'RoleController@store');
-    Route::post('/permisos', 'PermisoController@store');
-    Route::post('/usuarios', 'UsuarioController@store');
-    Route::post('/servicios', 'ServicioController@store');
-    Route::post('/actividades', 'ActividadController@store');
-    Route::post('/cargos', 'CargoController@store');
-    Route::post('/estados', 'EstadoController@store');
-    Route::post('/aplicaciones', 'AplicacionController@store');
-    Route::post('/tipos_aplicaciones', 'TipoAplicacionController@store');
-    Route::post('/aplicaciones_accesos', 'AplicacionAccesoController@store');
-    Route::post('/dominios', 'DominioController@store');
-    Route::post('/servidores', 'ServidorController@store');
-    Route::post('/servidores_accesos', 'ServidorAccesoController@store');
-    Route::post('/datacentros', 'DatacentroController@store');
-    Route::post('/sistemas_operativos', 'SistemaOperativoController@store');
-    Route::post('/tags', 'TagController@store');
-    Route::post('/usuarios_bitacora_servicios', 'UsuarioBitacoraServicioController@store');
-    Route::post('/clusters', 'ClusterController@store');
-});
-
-Route::group(['middleware' => ['u']], function(){
-    //Route::resource('/catalogo_servicios', 'CatalogoServicioController');
-    Route::put('/roles/{id}', 'RoleController@update');
-    Route::put('/permisos/{id}', 'PermisoController@update');
-    Route::put('/usuarios/{id}', 'UsuarioController@update');
-    Route::put('/servicios/{id}', 'ServicioController@update');
-    Route::put('/actividades/{id}', 'ActividadController@update');
-    Route::put('/cargos/{id}', 'CargoController@update');
-    Route::put('/estados/{id}', 'EstadoController@update');
-    Route::put('/aplicaciones/{id}', 'AplicacionController@update');
-    Route::put('/tipos_aplicaciones/{id}', 'TipoAplicacionController@update');
-    Route::put('/aplicaciones_accesos/{id}', 'AplicacionAccesoController@update');
-    Route::put('/dominios/{id}', 'DominioController@update');
-    Route::put('/servidores/{id}', 'ServidorController@update');
-    Route::put('/servidores_accesos/{id}', 'ServidorAccesoController@update');
-    Route::put('/datacentros/{id}', 'DatacentroController@update');
-    Route::put('/sistemas_operativos/{id}', 'SistemaOperativoController@update');
-    Route::put('/tags/{id}', 'TagController@update');
-    Route::put('/usuarios_bitacora_servicios/{id}', 'UsuarioBitacoraServicioController@update');
-    Route::put('/clusters/{id}', 'ClusterController@update');
-});
-
-
-Route::group(['middleware' => 'd'], function(){
-    //Route::resource('/catalogo_servicios', 'CatalogoServicioController');
-    Route::delete('/roles/{id}', 'RoleController@destroy');
-    Route::delete('/permisos/{id}', 'PermisoController@destroy');
-    Route::delete('/usuarios/{id}', 'UsuarioController@destroy');
-    Route::delete('/servicios/{id}', 'ServicioController@destroy');
-    Route::delete('/actividades/{id}', 'ActividadController@destroy');
-    Route::delete('/cargos/{id}', 'CargoController@destroy');
-    Route::delete('/estados/{id}', 'EstadoController@destroy');
-    Route::delete('/aplicaciones/{id}', 'AplicacionController@destroy');
-    Route::delete('/tipos_aplicaciones/{id}', 'TipoAplicacionController@destroy');
-    Route::delete('/aplicaciones_accesos/{id}', 'AplicacionAccesoController@destroy');
-    Route::delete('/dominios/{id}', 'DominioController@destroy');
-    Route::delete('/servidores/{id}', 'ServidorController@destroy');
-    Route::delete('/servidores_accesos/{id}', 'ServidorAccesoController@destroy');
-    Route::delete('/datacentros/{id}', 'DatacentroController@destroy');
-    Route::delete('/sistemas_operativos/{id}', 'SistemaOperativoController@destroy');
-    Route::delete('/tags/{id}', 'TagController@destroy');
-    Route::delete('/usuarios_bitacora_servicios/{id}', 'UsuarioBitacoraServicioController@destroy');
-    Route::delete('/clusters/{id}', 'ClusterController@destroy');
-
-});
-
-
-*/
